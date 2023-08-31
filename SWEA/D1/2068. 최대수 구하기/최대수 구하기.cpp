3bdc05488b3da43dc7248f516fc550a176ec3977
Hanjun Cho <106874576+joranzan@users.noreply.github.com>
2023-08-31 18:52:11 +0900

    [D1] Title: 최대수 구하기, Time: 5 ms, Memory: 13,544 KB -BaekjoonHub

--- SWEA/D1/2068. 최대수 구하기/최대수 구하기.cpp
+++ SWEA/D1/2068. 최대수 구하기/최대수 구하기.cpp
@@ -1,26 +1,26 @@
 #include<iostream>
 #include<vector>
 #include<algorithm>
-
+ 
 using namespace std;
-
+ 
 int main()
 {
-	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-	int T;
-	cin >> T;
-	for (int test = 1; test <= T; test++)
-	{
-		vector<int> num(10);
-		for (int i = 0; i < 10; i++)
-		{
-			cin >> num[i];
-		}
-		int answer = *max_element(num.begin(),num.end());
-
-		cout << "#" << test << " " << answer << "\n";
-
-	}
-	
-	return 0;
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    int T;
+    cin >> T;
+    for (int test = 1; test <= T; test++)
+    {
+        vector<int> num(10);
+        for (int i = 0; i < 10; i++)
+        {
+            cin >> num[i];
+        }
+        int answer = *max_element(num.begin(),num.end());
+ 
+        cout << "#" << test << " " << answer << "\n";
+ 
+    }
+     
+    return 0;
 }
