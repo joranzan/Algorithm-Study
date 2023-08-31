5e2f870cfdc4ad34b029cd02614cea8f1d918ae9
Hanjun Cho <106874576+joranzan@users.noreply.github.com>
2023-08-31 18:51:31 +0900

    [D1] Title: 홀수만 더하기, Time: 8 ms, Memory: 13,376 KB -BaekjoonHub

---
+++ SWEA/D1/2072. 홀수만 더하기/홀수만 더하기.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	int T;
+	cin >> T;
+	for (int test = 1; test <= T; test++)
+	{
+		vector<int> num(10);
+		int answer = 0;
+		for (int i = 0; i < 10; i++)
+		{
+			cin >> num[i];
+			if (num[i] % 2 != 0)
+			{
+				answer += num[i];
+			}
+		}
+		cout << "#" << test << " " << answer << "\n";
+	}
+	return 0;
+}
