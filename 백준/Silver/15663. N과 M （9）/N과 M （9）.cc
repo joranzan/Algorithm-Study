a260d7be1443bf43254489bd73513a408b8a47b8
Hanjun Cho <106874576+joranzan@users.noreply.github.com>
2023-09-12 09:00:02 +0900

    [Silver II] Title: N과 M (9), Time: 12 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.cc
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.cc
@@ -7,31 +7,38 @@ using namespace std;
 
 
 int N, M;
-vector<int> Input;
-int Total[10000] = { 0, };
-int Cnt[10000] = { 0, };
-vector<int> Num;
+int Input[10] = { 0, };   //해당 인덱스와 입력 숫자 매핑
+vector<int> Answer; //고른 숫자를 넣을 배열
+bool Visited[10] = { 0, };  //해당 인덱스를 방문했는지 여부
 
 
 void DFS(int depth) {
 	if (depth >= M) {
-		for (int i = 0; i < Num.size(); i++) {
-			cout << Num[i] << " ";
+		for (int i = 0; i < M; i++) {
+			cout << Answer[i] << " ";
 		}
 		cout << "\n";
 		return;
 	}
 	int lastNum = 0;
 
-	for (int i = 0; i < N; i++) {
-		if (lastNum == Input[i]) continue;
-		if (Cnt[Input[i]] + 1 > Total[Input[i]]) continue;
-		lastNum = Input[i];
-		Cnt[Input[i]]++;
-		Num.push_back(Input[i]);
+	for (int i = 0; i < N; i++) {   //0번 인덱스부터 N-1인덱스까지
+		if (Visited[i] == 1) continue; //해당 인덱스 방문했다면 무시
+		if (Input[i] == lastNum) continue; //인덱스는 다르지만 값이 같은 경우 무시
+		
+		
+		//마지막 숫자 갱신(다음 인덱스에도 같은 숫자일 수도 있기 때문에 처리하기 위해서)
+		lastNum = Input[i];    
+		
+		Visited[i] = true;
+		Answer.push_back(Input[i]); //정답 배열에 골라서 넣기
+		
 		DFS(depth + 1);
-		Num.pop_back();
-		Cnt[Input[i]]--;
+
+		Visited[i] = false;
+		Answer.pop_back();
+
+
 	}
 
 }
@@ -43,15 +50,13 @@ int main() {
 
 	for (int i = 0; i < N; i++) {
 		
-		int temp;
-		cin >> temp;
-		Input.push_back(temp);
-		Total[temp]++;
+		cin >> Input[i];
 	}
-
-	sort(Input.begin(), Input.end());
+	sort(Input, Input + N);
 
 	DFS(0);
 
+
 	return 0;
 }
+
