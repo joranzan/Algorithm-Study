5eddf1503dd3c39639d387adaac933130ac2e50a
joranzan <chohj0816@naver.com>
2023-09-15 17:59:09 +0900

    we

---
+++ BaaarkingDog_BOJ/12. Backtracking/15654. N과 M(5).cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main() {
+
+	int N, M;
+	cin >> N >> M;
+
+	vector<int> input;
+
+
+	for (int i = 0; i < N; i++) {
+		int temp;
+		cin >> temp;
+		input.push_back(temp);
+	}
+
+	// 이중으로
+
+	return 0;
+}
