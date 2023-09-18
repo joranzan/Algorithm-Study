124b9d543d02c1959e5e80c6afe43422a73b003f
joranzan <chohj0816@naver.com>
2023-09-18 17:59:41 +0900

    dsf

--- BaaarkingDog_BOJ/7. Deque/10866. 덱.cpp
+++ BaaarkingDog_BOJ/7. Deque/10866. 덱.cpp
@@ -1,8 +1,33 @@
 #include<iostream>
+#include<deque>
+#include<string>
 
 using namespace std;
 
 int main() {
+
+	int Q;
+	cin >> Q;
+
+	for (int i = 0; i < Q; i++) {
+
+		string s;
+		cin >> s;
+
+		if (s == "push_front") {
+
+		}
+		else if (s == "push_back") {
+
+		}
+		else if (s == "pop_front") {
+
+		}
+		else if (s == "pop_back") {
+
+		}
+
+	}
 
 	return 0;
 }
