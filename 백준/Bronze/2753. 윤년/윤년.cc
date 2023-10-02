f795c5e2622d72dbf88c8a908af6481cfe59482e
Hanjun Cho <106874576+joranzan@users.noreply.github.com>
2023-10-02 11:05:32 +0900

    [Bronze V] Title: 윤년, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.cc
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    
+    int n;
+    cin>>n;
+    if(n%400==0) cout<<"1";
+    else if(n%100==0) cout<<"0";
+    else if(n%4==0) cout<<"1";
+    else cout<<"0";
+    
+    return 0;
+}
