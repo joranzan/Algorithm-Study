472f0265dc611cc833a41a22d8b15370ddc1aa1e
joranzan <chohj0816@naver.com>
2023-10-20 17:56:27 +0900

    2342

--- BaaarkingDog_BOJ/13. Simulation/14500. 테트로미노.cpp
+++ BaaarkingDog_BOJ/13. Simulation/14500. 테트로미노.cpp
@@ -1,8 +1,283 @@
 #include<iostream>
+#include<algorithm>
+#include<queue>
 
 using namespace std;
 
+//문제분석
+
+//폴리오미노
+//정사각형 서로 겹치면 안됨
+//도형은 모두 연결되어있어야함(반드시 변끼리)
+//회전 / 대칭 가능
+
+
+int N, M;
+int Map[500][500] = { 0, };
+int Ans = 0;
+
+struct pos {
+	int row;
+	int col;
+};
+const int dr[4] = { -1,1,0,0 };
+const int dc[4] = { 0,0,-1,1 };
+bool Visited[500][500] = { false, };
+bool dirVisited[4] = { false, };
+
+//1. DFS로 푸는 방법
+
+//나머지 모양
+void DFS(int r, int c, int depth, int sum) {
+
+	if (depth >= 4) {
+		Ans = max(Ans, sum);
+		return;
+	}
+
+	for (int i = 0; i < 4; i++) {
+		int next_r = r + dr[i];
+		int next_c = c + dc[i];
+		if (next_r < 0 || next_c < 0 || next_r >= N || next_c >= M) continue;
+		if (Visited[next_r][next_c]) continue;
+		sum += Map[next_r][next_c];
+		Visited[next_r][next_c] = true;
+		DFS(next_r, next_c, depth + 1, sum);
+		sum -= Map[next_r][next_c];
+		Visited[next_r][next_c] = false;
+	}
+
+
+}
+//빠큐 모양
+void Permutation(int r, int c, int index, int depth, int sum) {
+	
+	if (depth >= 4) {
+		Ans = max(Ans, sum);
+		return;
+	}
+
+	for (int i = index; i < 4; i++) {
+		int next_r = r + dr[i];
+		int next_c = c + dc[i];
+		if (next_r < 0 || next_c < 0 || next_r >= N || next_c >= M) continue;
+		sum += Map[next_r][next_c];
+		Permutation(r, c, i + 1, depth + 1, sum);
+		sum -= Map[next_r][next_c];
+	}
+}
+
+
+//2. 구현으로 푸는 방법
+
+//대칭 같음   (1자)
+void shape1() {   
+
+		// 1 1 1 1
+
+		for (int i = 0; i < N; i++) {
+			int left = 0;
+			int right = 3;
+			int sum = 0;
+			for (int j = left; j <= right; j++) {
+				sum += Map[i][j];
+			}
+
+			while (right < M) {
+				
+				Ans = max(Ans, sum);
+
+				sum -= Map[i][left];
+				left++;
+				right++;
+				if (right >= M) break;
+				sum += Map[i][right];
+			}
+		}
+
+		//1
+		//1
+		//1
+		//1
+		for (int i = 0; i < M; i++) {
+			int left = 0;
+			int right = 3;
+			int sum = 0;
+			for (int j = left; j <= right; j++) {
+				sum += Map[j][i];
+			}
+
+			while (right < N) {
+
+				Ans = max(Ans, sum);
+
+				sum -= Map[left][i];
+				left++;
+				right++;
+				if (right >= N) break;
+				sum += Map[right][i];
+			}
+		}
+}
+
+//대칭 같음, 회전 같음  (정사각형)
+void shape2() {
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			if (i + 1 >= N) continue;
+			if (j + 1 >= M) continue;
+			int sum = Map[i][j] + Map[i + 1][j] + Map[i][j + 1] + Map[i + 1][j + 1];
+			Ans = max(Ans, sum);
+		}
+	}
+}
+
+//ㄴ자 + 빠큐 모양
+//대칭 다름, 회전 다름
+void shape3() {
+	// 1
+	// 1
+	// 1
+	for (int i = 0; i <= N-3; i++) {
+		for (int j = 0; j < M; j++) {
+			int sum = Map[i][j] + Map[i + 1][j] + Map[i + 2][j];	
+			//1 3 5
+			if (j - 1 >= 0) {
+				int add1 = sum + Map[i][j - 1];
+				int add2 = sum + Map[i + 1][j - 1];
+				int add3 = sum + Map[i + 2][j - 1];
+				Ans = max(Ans, add1);
+				Ans = max(Ans, add2);
+				Ans = max(Ans, add3);
+			}
+			//2 4 6
+			if (j + 1 < M) {
+				int add1 = sum + Map[i][j + 1];
+				int add2 = sum + Map[i + 1][j + 1];
+				int add3 = sum + Map[i + 2][j + 1];
+				Ans = max(Ans, add1);
+				Ans = max(Ans, add2);
+				Ans = max(Ans, add3);
+			}
+		}
+	}
+	
+	//1 1 1
+	for (int c = 0; c <= M - 3; c++) {
+		for (int r = 0; r < N; r++) {
+			int sum = Map[r][c] + Map[r][c + 1] + Map[r][c + 2];
+			//1 3 5
+			if (r - 1 >= 0) {
+				int add1 = sum + Map[r - 1][c];
+				int add2 = sum + Map[r - 1][c + 1];
+				int add3 = sum + Map[r - 1][c + 2];
+				Ans = max(Ans, add1);
+				Ans = max(Ans, add2);
+				Ans = max(Ans, add3);
+			}
+			//2 4 6
+			if (r + 1 < N) {
+				int add1 = sum + Map[r + 1][c];
+				int add2 = sum + Map[r + 1][c + 1];
+				int add3 = sum + Map[r + 1][c + 2];
+				Ans = max(Ans, add1);
+				Ans = max(Ans, add2);
+				Ans = max(Ans, add3);
+			}
+		}
+	}
+
+
+}
+
+//지그재그
+void shape4() {
+
+	for (int r = 0; r < N; r++) {
+		for (int c = 0; c < M; c++) {
+			if (r + 1 >= N) continue;
+			if (r + 2 >= N) continue;
+			if (c + 1 >= M) continue;
+			int sum = Map[r][c] + Map[r + 1][c] + Map[r + 1][c + 1] + Map[r + 2][c + 1];
+			Ans = max(Ans, sum);
+		}
+	}
+
+	for (int r = 0; r < N; r++) {
+		for (int c = 0; c < M; c++) {
+			if (c - 1 < 0) continue;
+			if (c + 1 >= M) continue;
+			if (r + 1 >= N) continue;
+			int sum = Map[r][c] + Map[r + 1][c - 1] + Map[r][c + 1] + Map[r + 1][c];
+			Ans = max(Ans, sum);
+		}
+	}
+
+	for (int r = 0; r < N; r++) {
+		for (int c = 0; c < M; c++) {
+			if (r - 1 < 0) continue;
+			if (r + 1 >= N) continue;
+			if (c + 1 >= M) continue;
+			int sum = Map[r][c] + Map[r - 1][c + 1] + Map[r][c + 1] + Map[r + 1][c];
+			Ans = max(Ans, sum);
+		}
+	}
+
+	for (int r = 0; r < N; r++) {
+		for (int c = 0; c < M; c++) {
+			if (r + 1 >= N) continue;
+			if (c + 1 >= M) continue;
+			if (c + 2 >= M) continue;
+			int sum = Map[r][c] + Map[r][c + 1] + Map[r + 1][c + 1] + Map[r + 1][c + 2];
+			Ans = max(Ans, sum);
+		}
+	}
+
+}
+
+
+
+void input() {
+	cin >> N >> M;
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			cin >> Map[i][j];
+		}
+	}
+}
+
+void solution() {
+
+	
+	/*
+	1. 구현으로 푸는 코드
+	shape1();
+	shape2();
+	shape3();
+	shape4();
+	*/
+
+	//2. DFS로 푸는 코드
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			Visited[i][j] = true;
+			DFS(i, j, 1, Map[i][j]);
+			Visited[i][j] = false;
+			Permutation(i, j, 0, 1, Map[i][j]);
+			int debugging = -1;
+		}
+	}
+
+
+	cout << Ans << "\n";
+}
+
 int main() {
+
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+	input();
+	solution();
 
 
 
---
+++ BaaarkingDog_BOJ/13. Simulation/15684. 사다리 조작.cpp
@@ -0,0 +1,220 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+
+//문제 분석
+//N : 세로선 (열)  M: 가로선(행)
+//인접한 세로선 사이에 가로선 놓을 수 있음
+//각각의 세로선마다 H개의 가로선 놓을 수 있음
+
+
+//두 가로선 연속 X
+//서로 접하면 X
+//점선 가로선 있는 위치에만 놓을 수 있음
+
+//게임 진행
+//세로선마다 진행
+//가장 위 -> 아래로 진행
+//가로선 만나면 옆칸 이동
+
+//목표 : i번 결과 i번
+
+struct pos {
+	int row;
+	int col;
+};
+
+struct Edge{
+	int rowNum;
+	int from;
+	int to;
+};
+
+int N, M;
+int H;
+
+int Answer = 2112345678;
+Edge Map[31][11];
+bool Visited[31][11] = { false, };
+
+vector<Edge> candi;
+
+
+
+bool check(int num) {
+
+	int nowRow = 1, nowCol = num;
+
+	if (Visited[1][3] && Visited[3][4] && Visited[4][2]) {
+		int debugging = -1;
+	}
+
+	while (nowRow <= H) {
+
+		if (nowCol == 1) {
+			if (!Visited[nowRow][nowCol]) {
+				nowRow++;
+				continue;
+			}
+		}
+		else if(nowCol==N){
+			if (!Visited[nowRow][nowCol - 1]) {
+				nowRow++;
+				continue;
+			}
+		}
+		else {
+			if (!Visited[nowRow][nowCol - 1] && !Visited[nowRow][nowCol]) {
+				nowRow++;
+				continue;
+			}
+		}
+		Edge nowEdge;
+		if (Visited[nowRow][nowCol]) {
+			nowEdge = Map[nowRow][nowCol];
+			nowCol = nowEdge.to;
+		}
+		else if (Visited[nowRow][nowCol - 1]) {
+			nowCol = nowCol - 1;
+		}
+		
+		nowRow++;
+	}
+
+	if (nowRow==H&&nowCol == num) return true;
+	else return false;
+}
+
+void SelectLine(int index, int depth) {
+
+	if (Visited[1][3] && Visited[3][4] && Visited[4][2]&&!Visited[6][1]) {
+		int debugging = -1;
+	}
+
+	if (Answer <= depth) return;
+
+	//탈출 조건
+	bool flag = true;
+	for (int i = 1; i <= N; i++) {
+		if (!check(i)) {
+			flag = false;
+			break;
+		}
+	}
+
+	if (flag) {
+		Answer = min(Answer, depth);
+		return;
+	}
+
+	
+
+	//사다리 설치
+	for (int i = index; i < candi.size(); i++) {
+		Edge next = candi[i];
+
+		if (next.from == 1) {
+			if (Visited[next.rowNum][next.from] || Visited[next.rowNum][next.from+1]) continue;
+		}
+		else if (next.from == N - 1) {
+			if (Visited[next.rowNum][next.from] || Visited[next.rowNum][next.from - 1]) continue;
+
+		}
+		else {
+			if (Visited[next.rowNum][next.from] || Visited[next.rowNum][next.from -1] || Visited[next.rowNum][next.from + 1]) {
+				continue;
+			}
+		}
+
+		Visited[next.rowNum][next.from] = true;
+		Map[next.rowNum][next.from] = { next.rowNum,next.from,next.from + 1 };
+		SelectLine(i + 1, depth + 1);
+		Map[next.rowNum][next.from] = { 0,0,0 };
+		Visited[candi[i].rowNum][candi[i].from] = false;
+	}
+
+
+}
+
+
+void input() {
+
+	cin >> N >> M; //1번부터 시작
+	cin >> H;
+
+	for (int i = 1; i <= M; i++) {
+		int a, b;  //a번 가로줄   b번 세로줄에서 b+1번 까지 연결
+		cin >> a >> b;
+
+		Map[a][b] = { a,b, b + 1 };
+		Visited[a][b] = true;
+
+	}
+
+	for (int i = 1; i <= H; i++) {
+		for (int j = 1; j <= N; j++) {
+			
+			if (j == 1) {
+				if (Visited[i][j]||Visited[i][j+1]) continue;
+			}
+			else if (j == N) {
+				continue;
+			}
+			else if (j == N - 1) {
+				if (Visited[i][j] || Visited[i][j - 1]) {
+					continue;
+				}
+			}
+			else {
+				if (Visited[i][j] || Visited[i][j - 1] || Visited[i][j + 1]) {
+					continue;
+				}
+			}
+			candi.push_back({ i, j, j + 1 });
+		}
+	}
+
+
+
+}
+
+void solution() {
+
+	if (M == 0) {
+		cout << "0\n";
+		return;
+	}
+	
+	bool already = true;
+	for (int i = 1; i <= N; i++) {
+		if (!check(i)) {
+			already = false;
+			break;
+		}
+	}
+
+	if (already) {
+		cout << "0\n";
+		return;
+	}
+
+	SelectLine(0, 0);
+	if (Answer == 2112345678) {
+		cout << "-1\n";
+	}
+	else cout << Answer << "\n";
+
+}
+
+
+
+int main() {
+
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	input();
+	solution();
+
+	return 0;
+}
