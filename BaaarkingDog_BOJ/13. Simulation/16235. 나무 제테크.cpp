a485db0a47f6b9fa5a220b158df4520227fc5e9e
joranzan <chohj0816@naver.com>
2023-10-25 19:49:22 +0900

    home

--- BaaarkingDog_BOJ/13. Simulation/16235. 나무 제테크.cpp
+++ BaaarkingDog_BOJ/13. Simulation/16235. 나무 제테크.cpp
@@ -2,6 +2,7 @@
 #include<algorithm>
 #include<vector>
 #include<queue>
+#include<deque>
 using namespace std;
 
 //문제 분석
@@ -52,47 +53,36 @@ struct tree {
 int N, M, K;
 int A[11][11] = { 0, };
 int Map[11][11] = { 0, };
-priority_queue<tree> Tree[11][11];
+deque<int> Tree[11][11];
 vector<tree> dead;   //죽은 나무들 모음
 int Ans = 0;
 
 void spring() {
 	//1. 봄
+	//나무 : 자신의 나이만큼 양분 섭취 후 나이++
+	//(여러개 나무일 때 나이가 어린나무부터 양분 먹음)
+	//땅에 양분 < 나무 나이 -> 죽음
 	for (int i = 1; i <= N; i++) {
 		for (int j = 1; j <= N; j++) {
-			if (Tree[i][j].empty()) continue;
-			int deadAge = 0;
-			priority_queue<tree> temp;
-			while (!Tree[i][j].empty()) {
-				//(여러개 나무일 때 나이가 어린나무부터 양분 먹음)
-				tree Now = Tree[i][j].top();
-				Tree[i][j].pop();
-
-				//땅에 양분 < 나무 나이 -> 죽음
-				if (Now.age > Map[i][j]) {
-					Now.alive = false;
-					dead.push_back(Now);
-					deadAge += (Now.age / 2);
-					continue;
+			int last = Tree[i][j].size();
+			for (int t = 0; t < Tree[i][j].size(); t++) {
+				if (Map[i][j] < Tree[i][j][t]) {
+					last = t;
+					break;
 				}
-				//나무 : 자신의 나이만큼 양분 섭취 후 나이++
-				Map[i][j] -= Now.age;
-				Now.age++;
-				temp.push(Now);
-
+				Map[i][j] -= Tree[i][j][t];
+				Tree[i][j][t]++;
+				
+			}
+			int deadNum = 0;
+			int T = Tree[i][j].size();
+			for (int t = 0; t < T - last; t++) {
+				if (Tree[i][j].size() == 0) continue;
+				deadNum += (Tree[i][j][Tree[i][j].size() - 1] / 2);
+				Tree[i][j].pop_back();
 			}
 
-			while (!Tree[i][j].empty()) {
-				tree Now = Tree[i][j].top();
-				Tree[i][j].pop();
-				Now.alive = false;
-				deadAge += (Now.age / 2);
-				dead.push_back(Now);
-			}
-
-			Tree[i][j] = temp;
-			//Map[i][j] += deadAge;
-			Map[i][j] += A[i][j];
+			Map[i][j] += deadNum;
 		}
 	}
 }
@@ -115,37 +105,29 @@ void autumn() {
 	const int dr[8] = { -1,1,0,0,-1,-1,1,1 };
 	const int dc[8] = { 0,0,-1,1,-1,1,-1,1 };
 
-	priority_queue<tree> temp[11][11];
+	vector<int> temp[11][11];
 
 	for (int i = 1; i <= N; i++) {
 		for (int j = 1; j <= N; j++) {
-			if (Tree[i][j].empty()) continue;
 			
-
-			while (!Tree[i][j].empty()) {
-				//조건 : 나무 나이 5의 배수
-				//인접한 8칸에 나무 생김 (땅 벗어나지 않는 곳만)
-
-				tree now = Tree[i][j].top();
-				Tree[i][j].pop();
-
-				temp[now.row][now.col].push(now);
-				if (now.age % 5 != 0) continue;
-				for (int i = 0; i < 8; i++) {
-					int next_r = now.row + dr[i];
-					int next_c = now.col + dc[i];
+			for (int t = 0; t < Tree[i][j].size(); t++) {
+				if (Tree[i][j][t] % 5 != 0) continue;
+				for (int dir = 0; dir < 8; dir++) {
+					int next_r = i + dr[dir];
+					int next_c = j + dc[dir];
 					if (next_r <= 0 || next_c <= 0 || next_r > N || next_c > N) continue;
 					tree next = { next_r, next_c , 1, true };
-					temp[next_r][next_c].push(next);
+					temp[next_r][next_c].push_back(1);
 				}
-
 			}
 		}
 	}
 
 	for (int i = 1; i <= N; i++) {
 		for (int j = 1; j <= N; j++) {
-			Tree[i][j] = temp[i][j];
+			for (int k = 0; k < temp[i][j].size(); k++) {
+				Tree[i][j].push_front(temp[i][j][k]);
+			}
 		}
 	}
 }
@@ -164,9 +146,9 @@ void winter() {
 void simulation() {
 
 	spring();
-	summer();
+	//summer();
 	autumn();
-	//winter();
+	winter();
 	
 }
 
@@ -174,6 +156,7 @@ void init() {
 	for (int i = 1; i <= N; i++) {
 		for (int j = 1; j <= N; j++) {
 			Map[i][j] = 5;
+			sort(Tree[i][j].begin(), Tree[i][j].end());
 		}
 	}
 }
@@ -182,7 +165,7 @@ void input() {
 	//입력
 	//N,M,K
 	cin >> N >> M >> K;
-	init();
+	
 	//A배열 (추가할 양분)
 	for (int i = 1; i <= N; i++) {
 		for (int j = 1; j <= N; j++) {
@@ -193,8 +176,10 @@ void input() {
 	for (int i = 0; i < M; i++) {
 		int r, c, old;
 		cin >> r >> c >> old;
-		Tree[r][c].push({ r,c,old,true });
+		Tree[r][c].push_back(old);
 	}
+
+	init();
 }
 
 void solution() {
