45f53878e318e7e9414f1f3f463f9b657fad2a13
joranzan <chohj0816@naver.com>
2023-11-14 18:00:32 +0900

    Dfsfsfds

---
+++ BaaarkingDog_BOJ/13. Simulation/17825. 주사위 윷놀이_2차원배열 풀.cpp
@@ -0,0 +1,168 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+
+using namespace std;
+
+//문제 분석
+
+//시작 : 말 4개
+
+//이동 조건
+	//1) 화살표 방향
+	//2) 파란색 칸 출발 : 파란색 화살표만
+	//3) 나머지 : 빨간 화살표
+	//4) 도착 칸 : 주사위 상관없이 종료
+
+// 10개의 턴 (1~5 주사위) 
+// 도착 안한 말 이동(주사위 수만큼)
+// 말이 이동하는 칸에 다른 말 있으면 그 말 못 고름 *********
+// 이동할 때마다 칸 점수 +=
+
+void input();
+
+int Dice[10] = { 0, };
+int Route[4][24] = { 0, };
+int Visited[4][24] = { 0, };
+int Index[4] = { 0, };
+int routeNow[4] = { 0, };
+int Answer = 0;
+
+
+void DFS(int depth, int sum) {
+
+	if (sum + (10 - depth) * 40 <= Answer) return;
+
+	if (depth >= 10) {
+		//Answer 갱신
+		Answer = max(Answer, sum);
+		return;
+	}
+
+	bool flag = true;
+	for (int i = 0; i < 4; i++) {
+		if (Index[i] != -1) {
+			flag = false;
+			break;
+		}
+	}
+	if (flag) {
+		Answer = max(Answer, sum);
+		return;
+	}
+
+	int nowDice = Dice[depth];
+
+	for (int i = 0; i < 4; i++) {
+		if (Index[i] == -1) continue;
+		int nowIndex = Index[i];
+		int route = routeNow[i];
+		int nextroute = route;
+		int nextIndex = nowIndex + nowDice;
+		if (nextIndex > 23 || Route[route][nextIndex] == 0) {
+			Visited[route][nowIndex] = 0;
+			Index[i] = -1;
+			DFS(depth + 1, sum);
+		}
+		else {
+			if (route == 0) {
+				if (Route[0][nextIndex] == 10) nextroute = 1;
+				else if (Route[0][nextIndex] == 20) nextroute = 2;
+				else if (Route[0][nextIndex] == 30) nextroute = 3;
+			}
+
+			// 9 10 11 12
+			// 13 14 15 16
+			// 19 20 21 22
+			if ((route == 1 && nextIndex == 9) || (route == 2 && nextIndex == 13) || (route == 3 && nextIndex == 19)) {
+				if (Visited[1][9] || Visited[2][13] || Visited[3][19]) continue;
+			}
+			else if ((route == 1 && nextIndex == 10) || (route == 2 && nextIndex == 14) || (route == 3 && nextIndex == 20)) {
+				if (Visited[1][10] || Visited[2][14] || Visited[3][20]) continue;
+			}
+			else if ((route == 1 && nextIndex == 11) || (route == 2 && nextIndex == 15) || (route == 3 && nextIndex == 21)) {
+				if (Visited[1][11] || Visited[2][15] || Visited[3][21]) continue;
+			}
+			else if ((route==0&&nextIndex==20)||(route == 1 && nextIndex == 12) || (route == 2 && nextIndex == 16) || (route == 3 && nextIndex == 22)) {
+				if (Visited[0][20] || Visited[1][12] || Visited[2][16] || Visited[3][22]) continue;
+			}
+
+
+			if (Visited[nextroute][nextIndex]==1||Visited[route][nextIndex]) continue;
+
+			routeNow[i] = nextroute;
+			Visited[nextroute][nextIndex] = 1;
+			Visited[route][nowIndex] = 0;
+			Index[i] = nextIndex;
+			DFS(depth + 1, sum + Route[nextroute][nextIndex]);
+		}		
+
+		//재귀 호출
+		
+
+		if (nextIndex > 22 || Route[route][nextIndex] == 0) {
+
+			Visited[route][nowIndex] = 1;
+			Index[i] = nowIndex;
+		}
+		else {
+			routeNow[i] = route;
+			Visited[route][nowIndex] = 1;
+			Visited[nextroute][nextIndex] = 0;
+			Index[i] = nowIndex;
+		}
+
+	}
+
+}
+
+void solution() {
+
+	int nextIndex = Dice[0];
+	if (Route[0][nextIndex] == 10) {
+		routeNow[0] = 1;
+	}
+
+	Index[0] = nextIndex;
+	Visited[routeNow[0]][nextIndex] = 1;
+
+
+	DFS(1,Route[0][nextIndex]);
+
+	cout << Answer;
+}
+
+
+int main() {
+
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+	input();
+	solution();
+
+	return 0;
+}
+
+
+void input() {
+	for (int i = 0; i < 10; i++) {
+		cin >> Dice[i];
+	}
+
+	for (int i = 1; i <= 20; i++) {
+		Route[0][i] = i * 2;
+		if (i <= 5) Route[1][i] = i * 2;
+		if (i <= 10) Route[2][i] = i * 2;
+		if (i <= 15) Route[3][i] = i * 2;
+	}
+	Route[1][6] = 13; Route[1][7] = 16; Route[1][8] = 19;
+	Route[3][16] = 28; Route[3][17] = 27; Route[3][18] = 26;
+	Route[2][11] = 22; Route[2][12] = 24; 
+	
+	Route[1][9] = 25; Route[1][10] = 30; Route[1][11] = 35; Route[1][12] = 40;
+	Route[2][13] = 25; Route[2][14] = 30; Route[2][15] = 35; Route[2][16] = 40;
+	Route[3][19] = 25; Route[3][20] = 30; Route[3][21] = 35; Route[3][22] = 40;
+
+
+}
+
---
+++ BaaarkingDog_BOJ/13. Simulation/19236. 청소년 상어.cpp
@@ -0,0 +1,204 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+//4x4
+
+//물고기 (번호(1~16) , 방향)
+
+//물고기 이동 (상어 배치 다음)
+//1) 이동 가능 칸 : 빈칸, 다른 물고기 칸
+//2) 이동 불가 칸 : 상어, 범위 밖
+//3) 이동방법 : 
+	//1. 이동할 수 있는 칸까지 45도 반시계
+	//2. 없으면 이동 X
+	//3. 다른 물고기 있으면 Swap (방향 유지)
+
+
+//상어 이동 (물고기 이동 끝난 후)
+// 초기 : (0,0) 물고기 먹고 (0,0)에 위치 
+// 
+//1) 물고기 있는 칸으로 도착한 경우
+	//1. 물고기 먹음
+	//2. 방향 뺏음
+
+//2) 이동 중 물고기 만나면 무시
+
+//3) 물고기 없는 칸으로 이동 X
+
+//4) 이동 가능한 칸 없으면 귀환
+
+//방향 8개
+void input();
+void movefish();
+
+
+struct pos {
+	int row;
+	int col;
+	int dir;
+};
+
+const int dr[9] = {0, -1,-1,0,1,1,1,0,-1 };
+const int dc[9] = {0, 0,-1,-1,-1,0,1,1,1 };
+int Map[4][4] = { 0, };
+
+int Dead[17] = { 0, };
+pos Fish[17];
+pos Shark;
+int Answer = 0;
+
+
+void DFS(int sum, int depth) {
+	//먹을 물고기 다 죽었거나
+	//상어 이동할 곳 없으면 끝
+	if (depth >= 15) {
+		Answer = max(Answer, sum);
+		return;
+	}
+	int sharkDir = Shark.dir;
+	int sharkRow = Shark.row;
+	int sharkCol = Shark.col;
+
+	movefish();
+	int Temp[4][4] = { 0, };
+
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			Temp[i][j] = Map[i][j];
+		}
+	}
+
+	vector<pair<int, int>> Candi;  //먹을 후보
+
+	while (1) {
+		sharkRow += dr[sharkDir];
+		sharkCol += dc[sharkDir];
+		
+		//3) 물고기 없는 칸으로 이동 X
+		if (sharkRow < 0 || sharkCol < 0 || sharkRow >= 4 || sharkCol >= 4) break;
+		if (Temp[sharkRow][sharkCol] == 0) break;
+		Candi.push_back({ sharkRow, sharkCol });
+	}
+	//이동 가능한 칸 없으면 귀환
+	if (Candi.size() == 0) {
+		Answer = max(Answer, sum);
+		return;
+	}
+	sharkRow = Shark.row;
+	sharkCol = Shark.col;
+
+	for (int i = 0; i < Candi.size(); i++) {
+
+		int nextRow = Candi[i].first;
+		int nextCol = Candi[i].second;
+		int nextFish = Map[nextRow][nextCol];
+		//상어 이동
+		Map[sharkRow][sharkCol] = 0;
+		Map[nextRow][nextCol] = -1;
+		//물고기 먹음
+		Dead[nextFish] = 1;
+		Shark.row = nextRow; 
+		Shark.col = nextCol; 
+		Shark.dir = Fish[nextFish].dir;
+		
+		//재귀 호출 부분
+		DFS(sum + nextFish, depth+1);
+
+		//상어 이동
+		
+		//물고기 먹음
+		Dead[nextFish] = 0;
+		Shark.row = sharkRow; Shark.col = sharkCol; Shark.dir = sharkDir;
+
+		for (int j = 0; j < 4; j++) {
+			for (int k = 0; k < 4; k++) {
+				Map[j][k] = Temp[j][k];
+			}
+		}
+
+	}
+
+}
+
+
+void solution() {
+	int init_Fish = Map[0][0];
+	Shark = { Fish[init_Fish].row, Fish[init_Fish].col, Fish[init_Fish].dir };
+	Map[0][0] = -1;  //상어 있는 칸 : -1
+	Dead[init_Fish] = 1;
+
+	DFS(init_Fish, 0);
+
+	cout << Answer;
+
+}
+
+int main() {
+
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+	input();
+	solution();
+
+	return 0;
+}
+
+
+
+void input() {
+
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			int a, b;
+			cin >> a >> b;
+			Fish[a] = { i,j,b };
+			Map[i][j] = a;
+		}
+	}
+
+}
+
+void movefish() {
+
+	for (int i = 1; i <= 16; i++) {
+		if (Dead[i] == 1) continue;  //죽은 물고기 무시
+
+		int nowRow = Fish[i].row;
+		int nowCol = Fish[i].col;
+		int nowDir = Fish[i].dir;
+
+		for (int d = nowDir; d < 8 + nowDir; d++) {
+			int nextDir = d;
+			if (nextDir > 8) nextDir -= 8;
+			int nextRow = nowRow + dr[nextDir];
+			int nextCol = nowCol + dc[nextDir];
+			//이동 불가 칸 : 상어, 범위 밖
+			//2. 없으면 이동 X
+			if (nextRow < 0 || nextCol < 0 || nextRow >= 4 || nextCol >= 4) continue;
+			if (Map[nextRow][nextCol] == -1) continue;
+
+			if (Map[nextRow][nextCol] == 0) {  //그냥 빈칸인 경우 이동
+				Map[nextRow][nextCol] = i;
+				Map[nowRow][nowCol] = 0;
+				Fish[i].row = nextRow;
+				Fish[i].col = nextCol;
+				Fish[i].dir = nextDir;
+				break;
+			}
+			else {    //다른 물고기가 존재하는 경우
+				//다른 물고기 있으면 Swap(방향 유지)
+				int nextFish = Map[nextRow][nextCol];
+				swap(Map[nextRow][nextCol], Map[nowRow][nowCol]);
+				Fish[i].row = nextRow;
+				Fish[i].col = nextCol;
+				Fish[i].dir = nextDir;
+				Fish[nextFish].row = nowRow;
+				Fish[nextFish].col = nowCol;
+				break;
+			}
+		}
+	}
+
+}
