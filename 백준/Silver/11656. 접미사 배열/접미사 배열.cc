5685497341e989d00d6d61990737ff19c7b413cc
Hanjun Cho <106874576+joranzan@users.noreply.github.com>
2023-12-05 11:32:51 +0900

    [Silver IV] Title: 접미사 배열, Time: 4 ms, Memory: 2548 KB -BaekjoonHub

---
+++ 백준/Silver/11656. 접미사 배열/접미사 배열.cc
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+
+using namespace std;
+
+int main() {
+	
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	string sarr[1000];
+	string s;
+	cin >> s;
+
+	for (int i = 0; i < s.size(); i++) {
+		string temp;
+		for (int j = i; j < s.size(); j++) {
+			temp.push_back(s[j]);
+		}
+		sarr[i] = temp;
+	}
+
+	sort(sarr, sarr + s.size());
+	for (int i = 0; i < s.size(); i++) {
+		cout << sarr[i] << "\n";
+	}
+
+	return 0;
+}
