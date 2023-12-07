a8cf9535cc4bce9b56000564953eb2ab1a1e01a2
Hanjun Cho <106874576+joranzan@users.noreply.github.com>
2023-12-07 19:19:34 +0900

    [Silver III] Title: 먹을 것인가 먹힐 것인가, Time: 28 ms, Memory: 2180 KB -BaekjoonHub

--- 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.cc
+++ 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.cc
@@ -7,43 +7,6 @@ int N, M;
 int arrN[20000];
 int arrM[20000];
 
-
-void solution() {
-	int cnt = 0;
-	for (int i = 0; i < N; i++) {
-		int nowNum = arrN[i];
-		if (nowNum <= arrM[0]) break;
-		if (nowNum > arrM[M-1]) {
-			cnt += M;
-			continue;
-		}
-		
-		cnt += (lower_bound(arrM, arrM + M, nowNum) - arrM);
-	}
-	cout << cnt << "\n";
-}
-
-void input() {
-	cin >> N >> M;
-
-	for (int i = 0; i < N; i++) {
-		cin >> arrN[i];
-	}
-	for (int i = 0; i < M; i++) {
-		cin >> arrM[i];
-	}
-
-	sort(arrN, arrN + N, greater<int>());
-	sort(arrM, arrM + M);
-
-}
-
-
-
-
-
-
-
 int main() {
 
 	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
@@ -53,8 +16,29 @@ int main() {
 
 	for (int test = 0; test < T; test++) {
 		
-		input();
-		solution();
+		cin >> N >> M;
+
+		for (int i = 0; i < N; i++) {
+			cin >> arrN[i];
+		}
+		for (int i = 0; i < M; i++) {
+			cin >> arrM[i];
+		}
+
+		sort(arrN, arrN + N, greater<int>());
+		sort(arrM, arrM + M);
+
+		int cnt = 0;
+		for (int i = 0; i < N; i++) {
+			int nowNum = arrN[i];
+			if (nowNum <= arrM[0]) break;
+			if (nowNum > arrM[M - 1]) {
+				cnt += M;
+				continue;
+			}
+			cnt += (lower_bound(arrM, arrM + M, nowNum) - arrM);
+		}
+		cout << cnt << "\n";
 	}
 
 
