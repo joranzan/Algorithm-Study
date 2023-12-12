6062fa81e2a0ebd4486c70e57f7a50d9ca5b335c
Hanjun Cho <106874576+joranzan@users.noreply.github.com>
2023-12-12 18:31:57 +0900

    [Silver III] Title: 계단 오르기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.cc
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.cc
@@ -2,40 +2,38 @@
 
 using namespace std;
 
+//밟은 계단의 최댓값 구하기
+// => 밟지 않은 계단의 최소값 구하기
+
+
+
 int N;
-int score[301] = { 0, };
-int DP[301][3] = { 0, };
+int Score[301] = { 0, };
+int sum = 0;
+int DP[301] = { 0, };
 
 int main() {
 
-	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
 
 	cin >> N;
 
 	for (int i = 1; i <= N; i++) {
-		cin >> score[i];
-	}
-	DP[N][1] = DP[N][2] = score[N];
-	DP[N - 1][1] = 0;
-	DP[N - 1][2] = DP[N][1] + score[N - 1];
-
-	for (int i = N-2; i >= 0; i--) {
-		DP[i][1] = max(DP[i + 2][2], DP[i + 2][1]) + score[i];
-		DP[i][2] = DP[i + 1][1] + score[i];
+		cin >> Score[i];
+		sum += Score[i];
 	}
 
-	int Answer =  max(DP[0][1], DP[0][2]);
-	Answer = max(Answer, DP[1][1]);
-	Answer = max(Answer, DP[1][2]);
+	DP[0] = 0;
+	DP[1] = Score[1];
+	DP[2] = Score[2];
+	DP[3] = Score[3];
 
-	cout << Answer;
+	for (int i = 4; i <= N - 1; i++) {
+		DP[i] = Score[i] + min(DP[i - 2], DP[i - 3]);
+	}
+
+	cout << sum - min(DP[N - 2], DP[N - 1]);
 
 	return 0;
 }
-
-//계단 오를때 1칸 또는 2칸
-//1칸씩 3번이상 금지
-
-//테이블 정의하기 (2차원 배열)
-// 
-//
