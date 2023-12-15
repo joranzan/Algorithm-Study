ee5885f9937cea59357e2b23ee9b8435ffe0ad45
Hanjun Cho <106874576+joranzan@users.noreply.github.com>
2023-12-15 16:00:45 +0900

    [Silver II] Title: 연속합, Time: 4 ms, Memory: 2800 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.cc
+++ 백준/Silver/1912. 연속합/연속합.cc
@@ -13,25 +13,37 @@ int main() {
 	int N;
 	cin >> N;
 
+	int Answer = Arr[0];
+
 	for (int i = 0; i < N; i++) {
 		cin >> Arr[i];
-	}
-
-	int Answer = Arr[0];
-	DP[0] = Arr[0];
-	for (int i = 1; i < N; i++) {
-
-		if (DP[i - 1] < 0) {
-			DP[i] = Arr[i];
+		if (i == 0) {
+			Answer = Arr[0];
 		}
-		else if (Arr[i] < 0) {
-			DP[i] = max(DP[i - 1] + Arr[i], Arr[i]);
+		else if (Arr[i - 1] < 0) {
+			Arr[i] = max(Arr[i - 1] + Arr[i], Arr[i]);
 		}
 		else {
-			DP[i] = Arr[i] + DP[i - 1];
+			Arr[i] += Arr[i - 1];
 		}
-		Answer = max(Answer, DP[i]);
+
+		Answer = max(Answer, Arr[i]);
 	}
+
+	//DP[0] = Arr[0];
+	//for (int i = 1; i < N; i++) {
+
+	//	if (DP[i - 1] < 0) {
+	//		DP[i] = Arr[i];
+	//	}
+	//	else if (Arr[i] < 0) {
+	//		DP[i] = max(DP[i - 1] + Arr[i], Arr[i]);
+	//	}
+	//	else {
+	//		DP[i] = Arr[i] + DP[i - 1];
+	//	}
+	//	Answer = max(Answer, DP[i]);
+	//}
 
 	cout << Answer;
 
