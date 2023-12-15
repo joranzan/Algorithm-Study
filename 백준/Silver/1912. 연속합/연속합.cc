5812274dcae1b2f12e97d0f58b29e187d700bcef
Hanjun Cho <106874576+joranzan@users.noreply.github.com>
2023-12-15 15:56:09 +0900

    [Silver II] Title: 연속합, Time: 8 ms, Memory: 2800 KB -BaekjoonHub

---
+++ 백준/Silver/1912. 연속합/연속합.cc
@@ -0,0 +1,39 @@
+#include<iostream>
+
+using namespace std;
+
+int Arr[100000] = { 0, };
+int DP[100000] = { 0, };
+
+
+int main() {
+
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+	int N;
+	cin >> N;
+
+	for (int i = 0; i < N; i++) {
+		cin >> Arr[i];
+	}
+
+	int Answer = Arr[0];
+	DP[0] = Arr[0];
+	for (int i = 1; i < N; i++) {
+
+		if (DP[i - 1] < 0) {
+			DP[i] = Arr[i];
+		}
+		else if (Arr[i] < 0) {
+			DP[i] = max(DP[i - 1] + Arr[i], Arr[i]);
+		}
+		else {
+			DP[i] = Arr[i] + DP[i - 1];
+		}
+		Answer = max(Answer, DP[i]);
+	}
+
+	cout << Answer;
+
+	return 0;
+}
