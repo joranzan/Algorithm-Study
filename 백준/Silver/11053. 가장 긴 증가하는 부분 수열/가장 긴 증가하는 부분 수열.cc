a05b2b03454d50d8bbb9dc8907ec43220372df91
Hanjun Cho <106874576+joranzan@users.noreply.github.com>
2023-12-17 15:05:36 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.cc
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<algorithm>
+
+using namespace std;
+
+int Arr[1001] = { 0, };
+int DP[1001] = { 0, };
+
+int main() {
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+	int N;
+	cin >> N;
+	for (int i = 1; i <= N; i++) {
+		cin >> Arr[i];
+		DP[i] = 1;
+	}
+
+	int Answer = 0;
+
+	for (int i = 1; i <= N; i++) {
+		for (int j = i + 1; j <= N; j++) {
+			if(Arr[j]>Arr[i]) DP[j] = max(DP[i] + 1, DP[j]);
+		}
+		Answer = max(Answer, DP[i]);
+	}
+
+	cout << Answer;
+
+	return 0;
+}
