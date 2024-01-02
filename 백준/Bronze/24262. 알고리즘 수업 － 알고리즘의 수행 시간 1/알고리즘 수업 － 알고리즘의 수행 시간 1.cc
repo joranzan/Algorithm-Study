834c46ed63e4e6bc5d879085e349b12454851a1d
Hanjun Cho <106874576+joranzan@users.noreply.github.com>
2024-01-02 19:01:10 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.cc
@@ -0,0 +1,13 @@
+#ifndef _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
+#endif
+/*
+24262_알고리즘 수업 - 알고리즘의 수행 시간 1
+1112KB	0ms
+*/
+#include <cstdio>
+
+int main() {
+	printf("1\n0\n");
+	return 0;
+}
