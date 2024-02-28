9233eee74ac31bb3e72e5ed39203648402e42f80
Hanjun Cho <106874576+joranzan@users.noreply.github.com>
2024-02-28 10:04:56 +0900

    [Silver II] Title: 주식, Time: 644 ms, Memory: 9832 KB -BaekjoonHub

--- 백준/Silver/11501. 주식/주식.cc
+++ 백준/Silver/11501. 주식/주식.cc
@@ -22,6 +22,8 @@ int main() {
 
 		int nowMax = 0;
 
+
+		//해당 날짜로부터 미래까지 주식값 중 최고점 저장
 		for (int i = N - 1; i >= 0; i--) {
 			if (nowMax < inputValue[i]) {
 				nowMax = inputValue[i];
@@ -30,55 +32,19 @@ int main() {
 			maxValue[i] = nowMax;
 		}
 
+		
 		for (int i = 0; i < N; i++) {
+			//당일 주식값이 고점이면 넘김
 			if (inputValue[i] == maxValue[i]) continue;
+			//앞으로 고점에서 팔 때 차익
 			Answer += (maxValue[i] - inputValue[i]);
 		}
 
-
-
-		int debugging = -1;
-
-
 		cout << Answer << "\n";
 
+		//전역변수 Answer TC마다 초기화
 		Answer = 0;
 	}
 
 	return 0;
 }
-
-
-/*
-
-1. 주식 하나를 산다.
-2. 원하는 만큼 가지고 있는 주식을 판다.
-3. 아무것도 안한다.
-
-*/
-
-
-/*
-
-최대일 때 팔아야함
-
-1 1 3 1 2
-
-3 3 3 2 2
-
-2+2+0+1+0
-
-
-10 7 6
-
-10 7 6
-
-0+0+0
-
-
-3 5 9
-
-9 9 9
-
-6+4+0
-*/
