81c1d0002f96d0911d8758f0c0a88b0de77a89da
Hanjun Cho <106874576+joranzan@users.noreply.github.com>
2024-05-28 12:29:39 +0900

    [Silver I] Title: 오르막 수, Time: 0 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Silver/11057. 오르막 수/오르막 수.cc
+++ 백준/Silver/11057. 오르막 수/오르막 수.cc
@@ -2,64 +2,49 @@
 
 using namespace std;
 
-int DAT[10] = { 0, };
-int DP[10][1002] = { 0, };
-int N;
-
-int Recursion(int digit, int num) {
-	if (digit == N) return DP[num][digit];
-
-	if (DP[num][digit] != 0) return DP[num][digit];
-
-	long long cnt = 0;
-	for (int i = num; i < 10; i++) {
-		cnt += Recursion(digit + 1, i);
-		if (cnt >= 10007) cnt %= 10007;
-	}
-	DP[num][digit] = cnt;
-	return cnt;
-}
+int DP[1001][10] = { 0, };
 
 int main() {
 
 	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 
+	int N;
 	cin >> N;
-	DAT[9] = 1;
+
 	
-	for (int i = 0; i <= 9; i++) {
-		DP[i][N] = 1;
-	}
+	for (int i = 1; i <= N; i++) DP[i][0] = 1;
 
-	//long long Answer = 0;
-	//for (int i = 0; i <= 9; i++) {
-	//	int ret = Recursion(1, i);
-	//}
-	//
-	//for (int i = 0; i <= 9; i++) {
-	//	Answer += DP[i][1];
-	//	if (Answer >= 10007) Answer %= 10007;
-	//}
+	for (int i = 0; i < 10; i++) DP[1][i] = i + 1;
 
-	//cout << Answer;
-
-	for (int digit = N; digit >= 1; digit--) {
-
-		if (digit != N) {
-			for (int num = 0; num <= 9; num++) {
-				DP[num][digit] = DP[num][digit + 1];
-			}
-		}
-		
-		for (int num = 8; num >= 0; num--) {
-			DP[num][digit] += DP[num + 1][digit];
-			if (DP[num][digit] >= 10007) DP[num][digit] %= 10007;
+	for (int i = 2; i <= N; i++) {
+		for (int j = 1; j < 10; j++) {
+			DP[i][j] += (DP[i - 1][j] + DP[i][j - 1]);
+			if (DP[i][j] >= 10007) DP[i][j] %= 10007;
 		}
 	}
+	
+	cout << DP[N][9];
 
-	cout << DP[0][1];
+
+	/*
+	  0 1 2 3 4 5 6 7 8 9
+    0 0 0 0 0 0 0 0 0 0 0
+	1 1 2 3 4 5 6 7 8 9 10
+	2 1 3 6 10 5 6 7 8 9 10
+	3 1 
+	4 1
+	5 1
+	6 1
+	7 1
+	8 1
+	9 1
+   10 1
+	
+	
+	*/
 
 
 
-	return 0;
+
+
 }
