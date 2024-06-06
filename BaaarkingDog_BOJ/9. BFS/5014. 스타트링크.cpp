ec62011ebfaca6010281d59659d178c01431d955
Hanjun Cho <chohj0816@naver.com>
2024-06-06 18:53:22 +0900

    0606 algorithm

---
+++ BaaarkingDog_BOJ/9. BFS/5014. 스타트링크.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+#include<queue>
+
+using namespace std;
+
+int Floor, Start, Goal, Up, Down;
+
+int Visited[1000001] = { 0, };
+int BFS() {
+
+	queue<int> q;
+	q.push(Start);
+	Visited[Start] = 1;
+
+	while (!q.empty()) {
+
+		int now = q.front();
+		q.pop();
+
+		if (now == Goal) {
+			return Visited[now] - 1;
+		}
+
+		int next_up = now + Up;
+		int next_down = now - Down;
+
+		if (next_up <= Floor&&Visited[next_up]==0) {
+			Visited[next_up] = Visited[now] + 1;
+			q.push(next_up);
+		}
+
+		if (next_down > 0 && Visited[next_down]==0) {
+			Visited[next_down] = Visited[now] + 1;
+			q.push(next_down);
+		}
+
+	}
+	return -1;
+}
+
+int main() {
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+	cin >> Floor >> Start >> Goal >> Up >> Down;
+
+	int answer = BFS();
+
+	if (answer == -1) cout << "use the stairs\n";
+	else cout << answer;
+
+	return 0;
+}
+
+/*
+¹®Á¦ ºÐ¼®
+
+ÃÑ FÃþ
+
+½ºÅ¸Æ®¸µÅ© : GÃþ
+
+°­È£ : SÃþ
+
+U¸¸Å­ D¸¸Å­
+
+*/
